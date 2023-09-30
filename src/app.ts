60e403e54ea35625d7e6d8ce4f8e240f97a824f8
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-09-30 04:45:56 +0300

    organize the Start Code

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,30 @@ import { getCorsAccess } from './Middleware/cors'
 import { Authenticate } from './Middleware/Auth'
 
 
-const path = require('path')
 var cors = require('cors')
 const express = require('express');
 const app = express()
 
+const path = require('path')
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 var jwt = require('jsonwebtoken');
 
-
-app.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-// app.use(cors()) 
+// Cors Access
 app.use(cors({
     origin:true,
     credentials:true
 }));
+
+//File Uplaoding
 app.use(fileUpload());
 
+// Using Json and Cookies
 app.use(express.json());
 app.use(cookieParser());
 app.set("view engine",'ejs')
 
-
+// Base endpoint to Test That API is Working
 app.get('/',async (req,res)=>{
     res.status(200).json("Welcome to API, It's Working")
 })
@@ -35,26 +35,13 @@ app.get('/',async (req,res)=>{
 //--------------------------------------------------------------------------------------------------------------------------
 //                                             Testing Functions Section
 //--------------------------------------------------------------------------------------------------------------------------  
-app.get("/test",async (req:any,res:any)=>{
-    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const userAgent = req.headers['user-agent'];
-  
-    console.log('Request received from:', clientIP);
-    console.log('User Agent:', userAgent);
 
-    res.status(200).json("Working")
-})
-app.get("/testAuth",getCorsAccess,Authenticate,async (req:any,res:any)=>{
-    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const userAgent = req.headers['user-agent'];
-    console.log("Cookies Check 1 For This Request: ",req.cookies)
-    console.log("Cookies Check 2 For This Request: ",req.headers['cookies'])
-    console.log('Request received from:', clientIP);
-    console.log('User Agent:', userAgent);
-
-    res.status(200).json("Working")
-})
 app.get("/checkauth",Authenticate,async (req:any,res:any)=>{
+    const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+    console.log('Request received from:', clientIP);
+    console.log('User Agent:', userAgent);
+
     res.status(200).json("authorized")
 
     
