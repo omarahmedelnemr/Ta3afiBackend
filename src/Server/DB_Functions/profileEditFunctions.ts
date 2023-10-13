02bc18bfe48e1ebf9dbfd10364172b405b496b9d
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-10-13 22:50:53 +0300

    Function part 1

--- src/Server/DB_Functions/profileEditFunctions.ts
+++ src/Server/DB_Functions/profileEditFunctions.ts
@@ -3,13 +3,79 @@ import { Doctor } from "../../entity/users/Doctor";
 import { LoginRouter } from "../../entity/login/LoginRouter";
 import checkUndefined from "../../middleFunctions/checkUndefined";
 import commonResposes from "../../middleFunctions/commonResposes";
-import { DoctorEducaion } from "../../entity/DoctorInfo/DoctorEducaion";
+import { DoctorEducation } from "../../entity/DoctorInfo/DoctorEducation";
 import { DoctorExperince } from "../../entity/DoctorInfo/DoctorExperince";
 import { DoctorCertificate } from "../../entity/DoctorInfo/DoctorCertificate";
 import SendMail from "../../middleFunctions/sendMail";
+import { ConfirmCode } from "../../entity/login/confirmationCode";
+import { Patient } from "../../entity/users/Patient";
 const bcrypt = require("bcrypt")
 
 class profileEditFunctions{
+    //---------------------------------------------------------------------------------------------
+    //------------------------------------ General Endpoints --------------------------------------
+    //---------------------------------------------------------------------------------------------
+
+    // Change Email Used in Login
+    async confirmChangeEmail(reqData){
+        // Check Parameter Existance
+        if (checkUndefined(reqData,["oldEmail","newEmail","code"])){
+            return commonResposes.missingParam
+        }
+        try{
+            // Get the Code Info and Check if it Even Exist
+            const code = await Database.getRepository(ConfirmCode).findOneBy({email:reqData['newEmail']})
+            if (code === null){
+                return commonResposes.notFound
+            }
+
+            // Check Code Expiration Date
+            const currentDate = new Date()
+            if (currentDate > code.expiresIn){
+                return commonResposes.sendError("Code Expired")
+            }
+
+            // Check if the Entered Code is Not Correct
+            else if (code.code !== reqData['code']){
+                return commonResposes.sendError("Wrong Code")
+            }
+            
+            // The Code is Correct
+            else{
+                const loginInfo = await Database.getRepository(LoginRouter).findOneBy({email:reqData["oldEmail"]})
+                if (loginInfo === null){
+                    return commonResposes.notFound
+                }   
+                loginInfo.email = reqData['newEmail']
+
+                // Remove Old Login Route 
+                await Database
+                .getRepository(LoginRouter)
+                .createQueryBuilder('login')
+                .delete()
+                .from(LoginRouter)
+                .where("email = :email", { email: reqData['oldEmail'] })
+                .execute()
+                
+                // Create New Login Route
+                await Database.getRepository(LoginRouter).save(loginInfo)
+
+                // Remove Confirmation Code from DB
+                await Database
+                .getRepository(ConfirmCode)
+                .createQueryBuilder('ConfirmCode')
+                .delete()
+                .from(ConfirmCode)
+                .where("email = :email", { email: reqData['newEmail'] })
+                .execute()
+
+                return commonResposes.done
+            }
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResposes.Error
+        }
+    }
 
     // Change Users Password
     async changePassword(reqData){
@@ -43,6 +109,100 @@ class profileEditFunctions{
         }
     }
 
+
+    //---------------------------------------------------------------------------------------------
+    //------------------------------------- Doctor Endpoints --------------------------------------
+    //---------------------------------------------------------------------------------------------
+
+    // Change Doctor Name
+    async changeDoctorName(reqData){
+        if (checkUndefined(reqData,['id','newName'])){
+            return commonResposes.missingParam
+        }
+        // Get Doctor's Info
+        const doctor = await Database.getRepository(Doctor).findOneBy({id:reqData['id']})
+        if (doctor === undefined){
+            return commonResposes.notFound
+        }
+
+        // Change Doctor's Name
+        doctor.name = reqData['newName']
+
+        // Save Changes To DB
+        await Database.getRepository(Doctor).save(doctor)
+
+        return commonResposes.done
+    }
+
+    // Change Doctor Title
+    async changeDoctorTitle(reqData){
+        if (checkUndefined(reqData,['id','newTitle'])){
+            return commonResposes.missingParam
+        }
+        // Get Doctor's Info
+        const doctor = await Database.getRepository(Doctor).findOneBy({id:reqData['id']})
+        if (doctor === undefined){
+            return commonResposes.notFound
+        }
+
+        // Change Doctor's Name
+        doctor.title = reqData['newTitle']
+
+        // Save Changes To DB
+        await Database.getRepository(Doctor).save(doctor)
+
+        return commonResposes.done
+    }
+    
+    // Change Doctor Birth Date
+    async changeDoctorBirthDate(reqData){
+        if (checkUndefined(reqData,['id','newBirthDate'])){
+            return commonResposes.missingParam
+        }
+        try{       
+            // Get Doctor's Info
+            const doctor = await Database.getRepository(Doctor).findOneBy({id:reqData['id']})
+            if (doctor === undefined){
+                return commonResposes.notFound
+            }
+
+            // Change Doctor's Name
+            doctor.birthDate = new Date(reqData['newBirthDate'])
+
+            // Save Changes To DB
+            await Database.getRepository(Doctor).save(doctor)
+            return commonResposes.done
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResposes.Error
+        }
+    }
+
+    // Edit Doctor's Description
+    async EditDescription(reqData){
+        // Check Parameter Existance
+        if (checkUndefined(reqData,["email","newDescription"])){
+            return commonResposes.missingParam
+        }
+        try{       
+            // Get Doctor's Info
+            const doctor = await Database.getRepository(Doctor).findOneBy({id:reqData['id']})
+            if (doctor === undefined){
+                return commonResposes.notFound
+            }
+
+            // Change Doctor's Name
+            doctor.description = reqData['newDescription']
+
+            // Save Changes To DB
+            await Database.getRepository(Doctor).save(doctor)
+            return commonResposes.done
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResposes.Error
+        }
+    }
+
     // Complete the Signup for Doctors (Education)
     async AddDoctorEducation(reqData){
         // Check Parameter Existance
@@ -61,16 +221,45 @@ class profileEditFunctions{
             const currentDoctor = await Database.getRepository(Doctor).findOneBy({id:LoginInfo.userID})
                         
             // Adding Doctors Educations
-            var newEducation     = new DoctorEducaion()
+            var newEducation     = new DoctorEducation()
             newEducation.doctor    = currentDoctor
             newEducation.title     = reqData['title']
             newEducation.place     = reqData['place']
             newEducation.startDate = new Date(reqData['startDate'])
             newEducation.endDate   = new Date(reqData['endDate'])
-            await Database.getRepository(DoctorEducaion).save(newEducation)
+            await Database.getRepository(DoctorEducation).save(newEducation)
             
             return commonResposes.done
 
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResposes.Error
+        }
+    }
+
+    // Delete Doctor Education
+    async DeleteDoctorEducation(reqData){
+        if(checkUndefined(reqData,['doctorID',"recordID"])){
+            return commonResposes.missingParam
+        }
+        try{
+            // Check Weather the Record Exist Or not
+            const EduRecord = await Database.getRepository(DoctorEducation).findOneBy({id:reqData["recordID"],doctor:{id:reqData['doctorID']}})
+            if(EduRecord === null){
+                return commonResposes.notFound
+            }
+
+            // Remove Confirmation Code from DB
+            await Database
+            .getRepository(DoctorEducation)
+            .createQueryBuilder('Education')
+            .delete()
+            .from(DoctorEducation)
+            .where("id = :recordID", { recordID: reqData['recordID'] })
+            .andWhere("doctor.id = doctorID",{ doctorID: reqData['doctorID']})
+            .execute()
+
+            return commonResposes.done
         }catch(err){
             console.log("Error!\n",err)
             return commonResposes.Error
@@ -111,6 +300,36 @@ class profileEditFunctions{
         }
     }
 
+    // Delete Doctor Experince
+    async DeleteDoctorExperince(reqData){
+        if(checkUndefined(reqData,['doctorID',"recordID"])){
+            return commonResposes.missingParam
+        }
+        try{
+
+            // Check Weather the Record Exist Or not
+            const ExpRecord = await Database.getRepository(DoctorExperince).findOneBy({id:reqData["recordID"],doctor:{id:reqData['doctorID']}})
+            if(ExpRecord === null){
+                return commonResposes.notFound
+            }
+
+            // Remove Confirmation Code from DB
+            await Database
+            .getRepository(DoctorExperince)
+            .createQueryBuilder('Experince')
+            .delete()
+            .from(DoctorExperince)
+            .where("id = :recordID", { recordID: reqData['recordID'] })
+            .andWhere("doctor.id = doctorID",{ doctorID: reqData['doctorID']})
+            .execute()
+
+            return commonResposes.done
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResposes.Error
+        }
+    }
+    
     // Complete the Signup for Doctors (Certificate)
     async AddDoctorCertificate(reqData){
         // Check Parameter Existance
@@ -144,6 +363,80 @@ class profileEditFunctions{
         }
     }
 
+    // Delete Doctor Certificate
+    async DeleteDoctorCertificate(reqData){
+        if(checkUndefined(reqData,['doctorID',"recordID"])){
+            return commonResposes.missingParam
+        }
+        try{
+
+            // Check Weather the Record Exist Or not
+            const CertRecord = await Database.getRepository(DoctorCertificate).findOneBy({id:reqData["recordID"],doctor:{id:reqData['doctorID']}})
+            if(CertRecord === null){
+                return commonResposes.notFound
+            }
+
+            // Remove Confirmation Code from DB
+            await Database
+            .getRepository(DoctorCertificate)
+            .createQueryBuilder('Certificate')
+            .delete()
+            .from(DoctorCertificate)
+            .where("id = :recordID", { recordID: reqData['recordID'] })
+            .andWhere("doctor.id = doctorID",{ doctorID: reqData['doctorID']})
+            .execute()
+
+            return commonResposes.done
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResposes.Error
+        }
+    }
+
+    
+    //---------------------------------------------------------------------------------------------
+    //------------------------------------- Patient Endpoints -------------------------------------
+    //---------------------------------------------------------------------------------------------
+
+    // Change Patient Name
+    async changePatientName(reqData){
+        if (checkUndefined(reqData,['id','newName'])){
+            return commonResposes.missingParam
+        }
+        // Get Patinet's Info
+        const patient = await Database.getRepository(Patient).findOneBy({id:reqData['id']})
+        if (patient === undefined){
+            return commonResposes.notFound
+        }
+
+        // Change Patient's Name
+        patient.name = reqData['newName']
+
+        // Save Changes To DB
+        await Database.getRepository(Patient).save(patient)
+
+        return commonResposes.done
+    }
+
+    // Change Patient BirthDate
+    async changePatientBirthDate(reqData){
+        if (checkUndefined(reqData,['id','newBirthDate'])){
+            return commonResposes.missingParam
+        }
+        // Get Patinet's Info
+        const patient = await Database.getRepository(Patient).findOneBy({id:reqData['id']})
+        if (patient === undefined){
+            return commonResposes.notFound
+        }
+
+        // Change Patient's Name
+        patient.birthDate =new Date(reqData['newBirthDate'])
+
+        // Save Changes To DB
+        await Database.getRepository(Patient).save(patient)
+
+        return commonResposes.done
+    }
 }
 
 export default new profileEditFunctions();
--- src/Server/profileEdits.ts
+++ src/Server/profileEdits.ts
@@ -1,4 +1,5 @@
 // Middle Functions
+import LoginFunctions from './DB_Functions/LoginFunctions'
 import profileEditFunctions from './DB_Functions/profileEditFunctions'
 
 //Main Modules
@@ -8,6 +9,18 @@ const router = Router()
 //---------------------------------------------------------------------------------------------
 //------------------------------------- General Endpoints -------------------------------------
 //---------------------------------------------------------------------------------------------
+
+// Send a Confirmation Code for Change The Email Used in Login Proccess for Both Doctor and Patient
+router.post("/send-confirmation-code",async (req,res)=>{
+    const response = await LoginFunctions.SendConfirmCode(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Change The Email Used in Login Proccess for Both Doctor and Patient
+router.post("/confirm-change-email",async (req,res)=>{
+    const response = await profileEditFunctions.confirmChangeEmail(req.body)
+    res.status(response['status']).json(response['data'])
+})
 
 // Change The Password for Both Doctor and Patient
 router.post("/change-password",async (req,res)=>{
@@ -20,9 +33,39 @@ router.post("/change-password",async (req,res)=>{
 //------------------------------------- Doctor Endpoints --------------------------------------
 //---------------------------------------------------------------------------------------------
 
+// Change Doctor's Name
+router.post("/doctor-name-edit",async (req,res)=>{
+    const response = await profileEditFunctions.changeDoctorName(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Change Doctor's Title
+router.post("/doctor-title-edit",async (req,res)=>{
+    const response = await profileEditFunctions.changeDoctorTitle(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Change Doctor's Birth Date
+router.post("/doctor-birthdate-edit",async (req,res)=>{
+    const response = await profileEditFunctions.changeDoctorBirthDate(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Change Doctor's Description
+router.post("/doctor-description-edit",async (req,res)=>{
+    const response = await profileEditFunctions.EditDescription(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
 // Adding New Education Record to a Doctor
 router.post("/doctor-education-record",async (req,res)=>{
     const response = await profileEditFunctions.AddDoctorEducation(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Removeing Education Record for a Doctor
+router.delete("/doctor-education-record",async (req,res)=>{
+    const response = await profileEditFunctions.DeleteDoctorEducation(req.body)
     res.status(response['status']).json(response['data'])
 })
 
@@ -32,18 +75,40 @@ router.post("/doctor-experince-record",async (req,res)=>{
     res.status(response['status']).json(response['data'])
 })
 
+// Removeing Experince Record for a Doctor
+router.delete("/doctor-experince-record",async (req,res)=>{
+    const response = await profileEditFunctions.DeleteDoctorExperince(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
 // Adding New Certificate Record to a Doctor
 router.post("/doctor-certificate-record",async (req,res)=>{
     const response = await profileEditFunctions.AddDoctorCertificate(req.body)
     res.status(response['status']).json(response['data'])
 })
 
+// Removeing Certificate Record for a Doctor
+router.delete("/doctor-certificate-record",async (req,res)=>{
+    const response = await profileEditFunctions.DeleteDoctorCertificate(req.body)
+    res.status(response['status']).json(response['data'])
+})
 
 
 //---------------------------------------------------------------------------------------------
-//------------------------------------- Patient Endpoints --------------------------------------
+//------------------------------------ Patient Endpoints --------------------------------------
 //---------------------------------------------------------------------------------------------
 
+// Change Doctor's Name
+router.post("/patient-name",async (req,res)=>{
+    const response = await profileEditFunctions.changePatientName(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Change Patient's Name
+router.post("/patient-birthdate",async (req,res)=>{
+    const response = await profileEditFunctions.changePatientBirthDate(req.body)
+    res.status(response['status']).json(response['data'])
+})
 
 
 
