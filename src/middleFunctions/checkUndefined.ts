51de78271dcf918a9ef2ef849081c735108c7ec3
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-10-15 21:11:00 +0300

    specify the MIssing Param in Console

--- src/middleFunctions/checkUndefined.ts
+++ src/middleFunctions/checkUndefined.ts
@@ -1,13 +1,16 @@
 // Check if Parameter are Defined in the Req Body or Req Query
 function checkUndefined(reqData,params){
-
+    const missing = []
     // Iterate on all Parameters and Check if Undefined
     for(var param of params){
         if (reqData[param] === undefined){
-
-            // Some Parameter are Undefined
-            return true
+            // Add Parameter to The List
+            missing.push(param)
         }
+    }
+    if (missing.length !== 0){
+        console.log(`Missing Parameters are: {${missing}}`)
+        return true
     }
 
     // All Parameters are There
