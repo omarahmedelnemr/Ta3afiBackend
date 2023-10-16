0ca19649a4f217509a036e8054ff293bfb3167a0
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-10-16 21:26:46 +0300

    File Managements

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,63 @@ app.get('/',async (req,res)=>{
     res.status(200).json("Welcome to API, It's Working")
 })
 
+//---------------------------------------------------------------------------------------------
+//------------------------------ Files Managment Endpoints ------------------------------------
+//---------------------------------------------------------------------------------------------
+
+// Upload Route
+app.post("/Upload",async (req,res)=>{
+    console.log('uploading a File!')
+    if (req.files ===null){
+        return res.status(400).send('No files were uploaded');
+
+    }
+    const file = req.files.file ===undefined ? req.files['files[]']:req.files.file;
+
+    try{
+        if (!file) {
+            return res.status(400).send('No files were uploaded');
+        }
+        const fileType = file.name.split('.')[file.name.split('.').length-1]
+        var filePath = ''
+        if (['pdf','doc','docx'].includes(fileType.toLowerCase())){
+            filePath = 'uploads/pdf'
+        }else if (['png','jpg','jpeg'].includes(fileType.toLowerCase())){
+            filePath = 'uploads/images'
+        }else{
+            return res.status("400").json('File Type is Not Supported')
+            
+        } 
+
+        const  d = new Date()
+        const fileName = String(d.getTime())+"."+fileType
+        file.mv(path.join(__dirname, filePath,fileName), (err:any) => {
+            if (err) {
+                return res.status(500).json({"message":"Server Error","error":err});
+            }
+            return res.status(200).json(fileName);
+        });
+    }catch(err){
+        console.log(err)
+        return res.status(500).json("Server Error")
+    }
+});
+
+// Show Files Route
+app.get("/file/:fileName",async (req,res)=>{
+    const fileName = req.params.fileName;
+    const fileType = fileName.split('.')[1]
+    if (fileType.toLowerCase() == 'pdf'){
+        const filePath = path.join(__dirname, 'uploads/pdf', fileName);
+        res.sendFile(filePath);
+
+    }else if (['png','jpg','jpeg'].includes(fileType.toLowerCase())){
+        const filePath = path.join(__dirname, 'uploads/images', fileName);
+        res.sendFile(filePath);
+    }else{
+        res.status(404).send('File Type Is Not Supported')
+    }
+});
 
 
 //--------------------------------------------------------------------------------------------------------------------------
