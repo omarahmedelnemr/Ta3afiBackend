f5a6b563a2a73e5f4131d22310614b84cc32f12c
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-11-04 08:15:32 +0200

    organize and Finish Blogs Endpoints

--- src/Server/blog.ts
+++ src/Server/blog.ts
@@ -7,11 +7,37 @@ import { Authenticate } from '../Middleware/Auth'
 import { Router } from 'express'
 const router = Router()
 
+//-------------------------------------------------------------------------------------------
+//------------------------------------- General ---------------------------------------------
+//-------------------------------------------------------------------------------------------
+
 // Get All category List
 router.get("/category-list",async (req,res)=>{
     const response = await blogsFunctions.GetCategoriesList(req.query)
     res.status(response['status']).json(response['data'])
 })
+
+// Search For all Posts Using category Delete a Comment like
+router.get("/article-list",async (req,res)=>{
+    const response = await blogsFunctions.GetAllArticleList(req.query)
+    res.status(response['status']).json(response['data'])
+})
+
+// Get a Single Article with Details
+router.get("/article",async (req,res)=>{
+    const response = await blogsFunctions.ViewAnArticle(req.query)
+    res.status(response['status']).json(response['data'])
+})
+
+// Get a Single Article with Details
+router.get("/article-by-name",async (req,res)=>{
+    const response = await blogsFunctions.SearchForArticleByName(req.query)
+    res.status(response['status']).json(response['data'])
+})
+
+//-------------------------------------------------------------------------------------------
+//------------------------------- Doctor Article Handling -----------------------------------
+//-------------------------------------------------------------------------------------------
 
 // Make a Doctor Request a New Category
 router.post("/request-new-category",async (req,res)=>{
@@ -20,14 +46,8 @@ router.post("/request-new-category",async (req,res)=>{
 })
 
 // Get All Article List for a Doctor
-router.get("/article-list",async (req,res)=>{
+router.get("/doctor-article-list",async (req,res)=>{
     const response = await blogsFunctions.GetDoctorArticleList(req.query)
-    res.status(response['status']).json(response['data'])
-})
-
-// Get a Single Article with Details
-router.get("/article",async (req,res)=>{
-    const response = await blogsFunctions.ViewAnArticle(req.query)
     res.status(response['status']).json(response['data'])
 })
 
@@ -44,9 +64,94 @@ router.post("/article-edit",async (req,res)=>{
 })
 
 // Make a Doctor Delete an Article
-router.delete("/request-new-category",async (req,res)=>{
+router.delete("/article",async (req,res)=>{
     const response = await blogsFunctions.DeleteArticle(req.body)
     res.status(response['status']).json(response['data'])
 })
+
+//-------------------------------------------------------------------------------------------
+//----------------------------------- Article Ranks -----------------------------------------
+//-------------------------------------------------------------------------------------------
+
+// Doctor Can Upvote for Other Doctor's Article
+router.post("/doctor-upvote-article",async (req,res)=>{
+    const response = await blogsFunctions.DoctorUpVote(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Make a Doctor Delete an Article Up Vote
+router.delete("/doctor-upvote-article",async (req,res)=>{
+    const response = await blogsFunctions.RemoveDoctorUpvote(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Doctor Can Upvote for Other Doctor's Article
+router.post("/patient-upvote-article",async (req,res)=>{
+    const response = await blogsFunctions.PatientUpVote(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Make a Doctor Delete an Article
+router.delete("/patient-upvote-article",async (req,res)=>{
+    const response = await blogsFunctions.RemovePatientUpvote(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+//-------------------------------------------------------------------------------------------
+//----------------------------------- Article Comments --------------------------------------
+//-------------------------------------------------------------------------------------------
+
+// Get Comments List For an Article
+router.get("/comment-list",async (req,res)=>{
+    const response = await blogsFunctions.GetArticleComments(req.query)
+    res.status(response['status']).json(response['data'])
+})
+
+// Doctor add Comment to an Article
+router.post("/doctor-comment",async (req,res)=>{
+    const response = await blogsFunctions.DoctorAddComment(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Doctor Delete his Comments
+router.delete("/doctor-comment",async (req,res)=>{
+    const response = await blogsFunctions.RemoveDoctorComment(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Doctor like a Comment
+router.post("/doctor-comment-like",async (req,res)=>{
+    const response = await blogsFunctions.DoctorLikeComment(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Doctor Delete a Comment like
+router.delete("/doctor-comment-like",async (req,res)=>{
+    const response = await blogsFunctions.RemoveCommentLike(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+//-------------------------------------------------------------------------------------------
+//---------------------------------- Article Saved List -------------------------------------
+//-------------------------------------------------------------------------------------------
+
+// Patient Get his Saved List
+router.get("/saved-list",async (req,res)=>{
+    const response = await blogsFunctions.GetAllSavedList(req.query)
+    res.status(response['status']).json(response['data'])
+})
+
+// Patient Save an Article to SavedList
+router.post("/save-article",async (req,res)=>{
+    const response = await blogsFunctions.saveArticleToSavedList(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Patient delete an Article front SavedList
+router.delete("/save-article",async (req,res)=>{
+    const response = await blogsFunctions.RemoveArticleFromSavedList(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
 
 export default router
