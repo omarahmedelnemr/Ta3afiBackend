cf061ec6091023ba5f1b39185d4a2a906bd3e6a8
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-11-11 22:35:07 +0200

    Add Authentication Middleware

--- src/Server/blog.ts
+++ src/Server/blog.ts
@@ -12,31 +12,31 @@ const router = Router()
 //-------------------------------------------------------------------------------------------
 
 // Get All category List
-router.get("/category-list",async (req,res)=>{
+router.get("/category-list", getCorsAccess, async (req,res)=>{
     const response = await blogsFunctions.GetCategoriesList(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Search For all Posts Using category
-router.get("/article-list",async (req,res)=>{
+router.get("/article-list", getCorsAccess, async (req,res)=>{
     const response = await blogsFunctions.GetAllArticleList(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Search for Articles By Using its Title
-router.get("/article-by-title",async (req,res)=>{
+router.get("/article-by-title", getCorsAccess, async (req,res)=>{
     const response = await blogsFunctions.SearchForArticleByTitle(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Get a Single Article with Details
-router.get("/article",async (req,res)=>{
+router.get("/article", getCorsAccess, async (req,res)=>{
     const response = await blogsFunctions.ViewAnArticle(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Get Comments List For an Article
-router.get("/comment-list",async (req,res)=>{
+router.get("/comment-list", getCorsAccess, async (req,res)=>{
     const response = await blogsFunctions.GetArticleComments(req.query)
     res.status(response['status']).json(response['data'])
 })
@@ -46,31 +46,31 @@ router.get("/comment-list",async (req,res)=>{
 //-------------------------------------------------------------------------------------------
 
 // Make a Doctor Request a New Category
-router.post("/request-new-category",async (req,res)=>{
+router.post("/request-new-category", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.RequestNewCategory(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Get All Article List for a Doctor
-router.get("/doctor-article-list",async (req,res)=>{
+router.get("/doctor-article-list", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.GetDoctorArticleList(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Make a Doctor Post an Article
-router.post("/article",async (req,res)=>{
+router.post("/article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.postNewArticle(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Make a Doctor Edit an Article
-router.post("/article-edit",async (req,res)=>{
+router.post("/article-edit", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.EditArticle(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Make a Doctor Delete an Article
-router.delete("/article",async (req,res)=>{
+router.delete("/article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.DeleteArticle(req.body)
     res.status(response['status']).json(response['data'])
 })
@@ -80,25 +80,25 @@ router.delete("/article",async (req,res)=>{
 //-------------------------------------------------------------------------------------------
 
 // Doctor Can Upvote for Other Doctor's Article
-router.post("/doctor-upvote-article",async (req,res)=>{
+router.post("/doctor-upvote-article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.DoctorUpVote(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Make a Doctor Delete an Article Up Vote
-router.delete("/doctor-upvote-article",async (req,res)=>{
+router.delete("/doctor-upvote-article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.RemoveDoctorUpvote(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Doctor Can Upvote for Other Doctor's Article
-router.post("/patient-upvote-article",async (req,res)=>{
+router.post("/patient-upvote-article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.PatientUpVote(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Make a Doctor Delete an Article
-router.delete("/patient-upvote-article",async (req,res)=>{
+router.delete("/patient-upvote-article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.RemovePatientUpvote(req.body)
     res.status(response['status']).json(response['data'])
 })
@@ -108,25 +108,25 @@ router.delete("/patient-upvote-article",async (req,res)=>{
 //-------------------------------------------------------------------------------------------
 
 // Doctor add Comment to an Article
-router.post("/doctor-comment",async (req,res)=>{
+router.post("/doctor-comment", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.DoctorAddComment(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Doctor Delete his Comments
-router.delete("/doctor-comment",async (req,res)=>{
+router.delete("/doctor-comment", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.RemoveDoctorComment(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Doctor like a Comment
-router.post("/doctor-comment-like",async (req,res)=>{
+router.post("/doctor-comment-like", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.DoctorLikeComment(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Doctor Delete a Comment like
-router.delete("/doctor-comment-like",async (req,res)=>{
+router.delete("/doctor-comment-like", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.RemoveCommentLike(req.body)
     res.status(response['status']).json(response['data'])
 })
@@ -137,19 +137,19 @@ router.delete("/doctor-comment-like",async (req,res)=>{
 
 
 // Patient Get his Saved List
-router.get("/saved-list",async (req,res)=>{
+router.get("/saved-list", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.GetAllSavedList(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Patient Save an Article to SavedList
-router.post("/save-article",async (req,res)=>{
+router.post("/save-article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.saveArticleToSavedList(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Patient delete an Article front SavedList
-router.delete("/save-article",async (req,res)=>{
+router.delete("/save-article", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await blogsFunctions.RemoveArticleFromSavedList(req.body)
     res.status(response['status']).json(response['data'])
 })
--- src/Server/community.ts
+++ src/Server/community.ts
@@ -12,25 +12,25 @@ const router = Router()
 //-------------------------------------------------------------------------------------------
 
 // Get All community List
-router.get("/community-list",async (req,res)=>{
+router.get("/community-list", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.GetCommunitiesList(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Search For all Posts Using community
-router.get("/post-feed",async (req,res)=>{
+router.get("/post-feed", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.GetPostsFeed(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Search for posts By Using its Title
-router.get("/post-by-text",async (req,res)=>{
+router.get("/post-by-text", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.SearchForPostByText(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Get All posts That A Spisific Patient Posts
-router.get("/patient-posts",async (req,res)=>{
+router.get("/patient-posts", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.GetPatientPosts(req.query)
     res.status(response['status']).json(response['data'])
 })
@@ -40,19 +40,19 @@ router.get("/patient-posts",async (req,res)=>{
 //-------------------------------------------------------------------------------------------
 
 // Get Comments List For an post
-router.post("/new-post",async (req,res)=>{
+router.post("/new-post", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.WritePost(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Edit an Existing Post
-router.post("/edit-post",async (req,res)=>{
+router.post("/edit-post", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.EditPost(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Delete a Post
-router.delete("/post",async (req,res)=>{
+router.delete("/post", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.DeletePost(req.body)
     res.status(response['status']).json(response['data'])
 })
@@ -62,43 +62,43 @@ router.delete("/post",async (req,res)=>{
 //-------------------------------------------------------------------------------------------
 
 // Add a Reaction on a Post
-router.post("/post-reaction",async (req,res)=>{
+router.post("/post-reaction", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.ReactOnPost(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Remove a Reaction from a Post
-router.delete("/post-reaction",async (req,res)=>{
+router.delete("/post-reaction", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.RemoveReactOnPost(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Get Comments List For an post
-router.get("/comment-list",async (req,res)=>{
+router.get("/comment-list", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.GetPostComments(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Add a Comment on a Post
-router.post("/post-comment",async (req,res)=>{
+router.post("/post-comment", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.AddComment(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Remove a Comment from a Post
-router.delete("/post-comment",async (req,res)=>{
+router.delete("/post-comment", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.RemoveComment(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Add a Reaction on a Comment
-router.post("/post-comment-reaction",async (req,res)=>{
+router.post("/post-comment-reaction", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.ReactOnComment(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Remove  a Reaction From a Comment
-router.delete("/post-comment-reaction",async (req,res)=>{
+router.delete("/post-comment-reaction", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.RemoveCommentReaction(req.body)
     res.status(response['status']).json(response['data'])
 })
@@ -108,19 +108,19 @@ router.delete("/post-comment-reaction",async (req,res)=>{
 //-------------------------------------------------------------------------------------------
 
 // Get All Saved-List Posts
-router.get("/save-post-list",async (req,res)=>{
+router.get("/save-post-list", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.GetAllSavedList(req.query)
     res.status(response['status']).json(response['data'])
 })
 
 // Add a Post to Saved List
-router.post("/save-post",async (req,res)=>{
+router.post("/save-post", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.savePostToSavedList(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Remove a Post from Saved List
-router.delete("/save-post",async (req,res)=>{
+router.delete("/save-post", getCorsAccess, Authenticate, async (req,res)=>{
     const response = await CommunityFunctions.RemovePostFromSavedList(req.body)
     res.status(response['status']).json(response['data'])
 })
