5d08f34a710126ec58ae32bd141b2ffaae86d1e7
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-12-03 20:43:33 +0200

    Login Refactor

--- src/Server/DB_Functions/LoginFunctions.ts
+++ src/Server/DB_Functions/LoginFunctions.ts
@@ -3,10 +3,9 @@ import { Doctor } from "../../entity/users/Doctor";
 import { LoginRouter } from "../../entity/login/LoginRouter";
 import { Patient } from "../../entity/users/Patient";
 import { Admin } from "../../entity/users/admin";
-import CommonResponse from "../../middleFunctions/commonResposes";
+import commonResponse from "../../middleFunctions/responseGenerator";
 import checkUndefined from "../../middleFunctions/checkUndefined";
 import SendMail from "../../middleFunctions/sendMail";
-import commonResposes from "../../middleFunctions/commonResposes";
 import { ConfirmCode } from "../../entity/login/confirmationCode";
 import { PatientAccountInfo } from "../../entity/PatientInfo/patientAccountInfo";
 const bcrypt = require("bcrypt")
@@ -38,24 +37,22 @@ class LoginFunctions{
 
         // Handle Missing Param
         if (checkUndefined(loginData,["email","password"])){
-            return CommonResponse.missingParam
+            return commonResponse.missingParam
         }
 
         // Check if the User Exist
         const User = await Database.getRepository(LoginRouter).findOneBy({email:loginData['email']})
         if (User === null){
-            return CommonResponse.notFound
+            return commonResponse.notFound
         }
-        // Check Password Correctness
-        else if (! await bcrypt.compare(loginData['password'], User['password'])){
-            return CommonResponse.wrongPassword
+        else if(! await bcrypt.compare(loginData['password'], User['password'])){  // Check Password Correctness
+            return commonResponse.wrongPassword
         }
-
-        // Check if the Account is Deleted or Not Completed
-        else if(User.active === false){
-            return commonResposes.sendError("This Account Was Deleted, Please Create New Account")
-        }else if (User.userID === -1 ){
-            return commonResposes.sendError("Your Account is Not Complete Yet, Please Complete and Try Again")
+        else if(User.active === false){    // Check if the Account is Deleted
+            return commonResponse.sendError("This Account Was Deleted, Please Create New Account")
+        }
+        else if (User.confirmed === false){ // Check if the Account is Not confirmed
+            return commonResponse.sendError("Your Account is Not Verified, Please Verify it and Try Again")
         }
 
         // Build The JWT and Return User's Info
@@ -86,27 +83,36 @@ class LoginFunctions{
             userInfo['role'] = User['role']
             userInfo['jwt'] = genratedJWT
 
-            return CommonResponse.sendData(userInfo)
+            return commonResponse.sendData(userInfo)
         }catch(err){
             console.log(err)
-            return CommonResponse.Error
+            return commonResponse.Error
         }
 
     }
 
-    // General Signup using Just Email and Password
-    async Signup(reqData){
-
+    // Create New Doctor Account
+    async doctorSignup(reqData){
         // Check Parameter Existance
-        if (checkUndefined(reqData,["email","password","role"])){
-            return commonResposes.missingParam
+        const checkParam = checkUndefined(reqData,['email','password',"name","birthDate","gender","title","description"])
+        if (checkParam){
+            return commonResponse.sendMissingParam(checkParam)
         }
         try{
             // Check if the Email Already in User
             const checkExist = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email'],active:true})
             if (checkExist !== null ){
-                return commonResposes.alreadyExist
+                return commonResponse.alreadyExist
             }
+            
+            // Create New Doctor Entity
+            const newDoctor        = new Doctor()
+            newDoctor.name         = reqData['name']
+            newDoctor.title        = reqData['title']
+            newDoctor.description  = reqData['description']
+            newDoctor.birthDate    = new Date(reqData['birthDate'])
+            newDoctor.gender       = reqData['gender']
+            await Database.getRepository(Doctor).save(newDoctor)
             
             // Create New Login Route with Role Check
             const newLoginRouter     = new LoginRouter()
@@ -114,32 +120,52 @@ class LoginFunctions{
             newLoginRouter.password  = await  bcrypt.hash(reqData['password'],10)
             newLoginRouter.active    = true
             newLoginRouter.confirmed = false
-            if (reqData['role'].toLowerCase() === 'doctor' || reqData['role'].toLowerCase() === 'patient'){
-                newLoginRouter.role     = reqData["role"].toLowerCase()
-            }else{
-
-                // The role is Entered Wrong
-                return commonResposes.Error
-            }
-            // Base UserID Number
-            newLoginRouter.userID   = -1
+            newLoginRouter.role     = 'doctor'
+            newLoginRouter.userID   = newDoctor.id
             await Database.getRepository(LoginRouter).save(newLoginRouter)
 
-
-            // Create A Confirmation Code and Store it in DB
-            const fourDigitsVerficationConde = `${Math.round(Math.random()*9)}${Math.round(Math.random()*9)}${Math.round(Math.random()*9)}${Math.round(Math.random()*9)}`
-            const newVerCode = new ConfirmCode()
-            newVerCode.email = reqData['email']
-            newVerCode.code  = fourDigitsVerficationConde
-            newVerCode.expiresIn =  new Date(new Date().getTime() + 2 * 60 * 1000);
-            await Database.getRepository(ConfirmCode).save(newVerCode)
-
-            // Send Email with the Confirmation Code
-            SendMail(reqData['email'],"Confirm Your Email",`Welcome To Ta3afi, here your Code to Verify Your Email ${fourDigitsVerficationConde}`)
-            return commonResposes.sendData("Confirmation Code Sent")
+            return commonResponse.done
         }catch(err){
             console.log("Error!\n",err)
-            return commonResposes.Error
+            return commonResponse.Error
+        }
+    }
+
+    // Create New Patient Account
+    async patientSignup(reqData){
+        // Check Parameter Existance
+        const checkParam = checkUndefined(reqData,['email','password',"name","birthDate","gender"])
+        if (checkParam){
+            return commonResponse.sendMissingParam(checkParam)
+        }
+        try{
+            // Check if the Email Already in User
+            const checkExist = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email'],active:true})
+            if (checkExist !== null ){
+                return commonResponse.alreadyExist
+            }
+            
+            // Create New Patient Entity
+            const newPatient        = new Patient()
+            newPatient.name         = reqData['name']
+            newPatient.birthDate    = new Date(reqData['birthDate'])
+            newPatient.gender       = reqData['gender']
+            await Database.getRepository(Patient).save(newPatient)
+            
+            // Create New Login Route with Role Check
+            const newLoginRouter     = new LoginRouter()
+            newLoginRouter.email     = reqData['email']
+            newLoginRouter.password  = await  bcrypt.hash(reqData['password'],10)
+            newLoginRouter.active    = true
+            newLoginRouter.confirmed = false
+            newLoginRouter.role     = 'patient'
+            newLoginRouter.userID   = newPatient.id
+            await Database.getRepository(LoginRouter).save(newLoginRouter)
+
+            return commonResponse.done
+        }catch(err){
+            console.log("Error!\n",err)
+            return commonResponse.Error
         }
     }
 
@@ -148,7 +174,7 @@ class LoginFunctions{
 
         // Check Parameter Existance
         if (checkUndefined(reqData,["email"])){
-            return commonResposes.missingParam
+            return commonResponse.missingParam
         }
         try{
             
@@ -174,44 +200,42 @@ class LoginFunctions{
 
             // Send Email With the Code
             SendMail(reqData['email'],"Confirm Your Email",`Welcome To Ta3afi,\nhere is your Code to Confirm Your Email: ${fourDigitsVerficationConde}`)
-            return commonResposes.sendData("Confirmation Code Sent")
+            return commonResponse.sendData("Confirmation Code Sent")
         }catch(err){
             console.log("Error!\n",err)
-            return commonResposes.Error
+            return commonResponse.Error
         }
     }
-
+    
     // Check if the Code is Correct and on Date
     async verifyCode(reqData){
 
         // Check Parameter Existance
         if (checkUndefined(reqData,["email","code"])){
-            return commonResposes.missingParam
+            return commonResponse.missingParam
         }
         try{
             // Get the Code Info and Check if it Even Exist
             const code = await Database.getRepository(ConfirmCode).findOneBy({email:reqData['email']})
             if (code === null){
-                return commonResposes.notFound
+                return commonResponse.notFound
             }
 
             // Check Code Expiration Date
             const currentDate = new Date()
             if (currentDate > code.expiresIn){
-                return commonResposes.sendError("Code Expired")
+                return commonResponse.sendError("Code Expired")
             }
 
             // Check if the Entered Code is Not Correct
             else if (code.code !== reqData['code']){
-                return commonResposes.sendError("Wrong Code")
+                return commonResponse.sendError("Wrong Code")
             }
             
             // The Code is Correct
             else{
-                // Update the Confirmed Status on Login Router
-                const loginRoute = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email']})
-                loginRoute.confirmed = true
-                await Database.getRepository(LoginRouter).save(loginRoute)
+                //  Createing a Auth Token (JWT), Valid Within half an Hour
+                const token = jwt.sign( {"email":reqData['email']},process.env.tokenSecret,{ expiresIn: 60 * 60 * 0.5 } ) 
 
                 // Remove Confirmation Code from DB
                 await Database
@@ -221,165 +245,78 @@ class LoginFunctions{
                 .from(ConfirmCode)
                 .where("email = :email", { email: reqData['email'] })
                 .execute()
-                
-                // Return to the FrontEnd
-                return commonResposes.sendData("Code Verified")
+
+                return commonResponse.sendData({"token":token})
+
             }
         }catch(err){
             console.log("Error!\n",err)
-            return commonResposes.Error
+            return commonResponse.Error
         }
     }
 
-    // Complete the Signup for Doctors (main Info)
-    async addDoctorMainInfo(reqData){
-        // Check Parameter Existance
-        if (checkUndefined(reqData,["email","name","birthDate","gender","title","description"])){
-            return commonResposes.missingParam
+    // Check if the Code is Correct and on Date
+    async confirmAccount(reqData){
+        const checkParam = checkUndefined(reqData,['email','token'])
+        if (checkParam){
+            return commonResponse.sendMissingParam(checkParam)
         }
+
         try{
-            const loginInfo = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email']})
-            if (loginInfo === null){
-                return commonResposes.notFound
-            }
-
-            // Create New Doctor Entity
-            const newDoctor        = new Doctor()
-            newDoctor.name         = reqData['name']
-            newDoctor.title        = reqData['title']
-            newDoctor.description  = reqData['description']
-            newDoctor.birthDate    = new Date(reqData['birthDate'])
-            newDoctor.gender       = reqData['gender']
-            await Database.getRepository(Doctor).save(newDoctor)
-            
-            // Update the User ID on the Login Route
-            loginInfo.userID = newDoctor.id
-            await Database.getRepository(LoginRouter).save(loginInfo)
-
-            return commonResposes.done
-        }catch(err){
-            console.log("Error!\n",err)
-            return commonResposes.Error
-        }
-    }
-
-    // Complete the Signup for Patients (main Info)
-    async addPatientMainInfo(reqData){
-        // Check Parameter Existance
-        if (checkUndefined(reqData,["email","name","birthDate","gender"])){
-            return commonResposes.missingParam
-        }
-        try{
-            // Check if Email Exist
-            const loginInfo = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email']})
-            if (loginInfo === null){
-                return commonResposes.notFound
-            }
-
-            // Create New Patient Entity
-            const newPatient        = new Patient()
-            newPatient.name         = reqData['name']
-            newPatient.birthDate    = new Date(reqData['birthDate'])
-            newPatient.gender       = reqData['gender']
-            await Database.getRepository(Patient).save(newPatient)
-            
-            // Update the User ID on the Login Route
-            loginInfo.userID = newPatient.id
-            await Database.getRepository(LoginRouter).save(loginInfo)
-
-            // Create an Empty Account Info
-            const newAccountInfo = new PatientAccountInfo()
-            newAccountInfo.patient = newPatient
-            await Database.getRepository(PatientAccountInfo).save(newAccountInfo)
-
-            return commonResposes.done 
-        }catch(err){
-            console.log("Error!\n",err)
-            return commonResposes.Error
-        }
-    }
-
-    // Check If the Sent Code is Good For Reset Password Opertation
-    async ConfirmForgetPasswordCode(reqData){
-        // Check Parameter Existance
-        if (checkUndefined(reqData,["email",'code'])){
-            return commonResposes.missingParam
-        }
-        try{
-            // Get the Code Info and Check if it Even Exist
-            const code = await Database.getRepository(ConfirmCode).findOneBy({email:reqData['email']})
-            if (code === null){
-                return commonResposes.notFound
-            }
-
-            // Check Code Expiration Date
-            const currentDate = new Date()
-            if (currentDate > code.expiresIn){
-                return commonResposes.sendError("Code Expired")
-            }
-
-            // Check if the Entered Code is Not Correct
-            else if (code.code !== reqData['code']){
-                return commonResposes.sendError("Wrong Code")
-            }
-            
-            // The Code is Correct
-            else{
-                // Generate a Secret JWT Code for Reset Password Operation that Last For Two Hours
-                const JWTData = {
-                    "email":reqData['email']
+            // Validate the Token 
+            try{
+                const token = await await jwt.verify(reqData['token'],process.env.tokenSecret)
+                if (token['email'] !== reqData['email']){
+                    throw new Error("Invalid JWT")
                 }
-                const token = jwt.sign( JWTData,process.env.JWTsecret,{ expiresIn: 60 * 60 *2 } ) 
-
-                // Remove Confirmation Code from DB
-                await Database
-                .getRepository(ConfirmCode)
-                .createQueryBuilder('ConfirmCode')
-                .delete()
-                .from(ConfirmCode)
-                .where("email = :email", { email: reqData['email'] })
-                .execute()
-                
-                // Return to the FrontEnd
-                return commonResposes.sendData({message:"Code Verified",secretCode:token})
+            }catch{
+                return commonResponse.custom(405,"unAuthenticated")
             }
+
+            // Update the Confirmed Status on Login Router
+            const loginRoute = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email']})
+            loginRoute.confirmed = true
+            await Database.getRepository(LoginRouter).save(loginRoute)
+            
+            // Return to the FrontEnd
+            return commonResponse.sendData("Account Verified")
         }catch(err){
             console.log("Error!\n",err)
-            return commonResposes.Error
+            return commonResponse.Error
         }
     }
-
+    
     // Reset The Password After Authorization Check
-    async ResetPassword(reqData){
+    async forgetPassword(reqData){
         // Check Parameter Existance
-        if (checkUndefined(reqData,["email",'secretCode',"newPassword"])){
-            return commonResposes.missingParam
+        if (checkUndefined(reqData,["email",'token',"newPassword"])){
+            return commonResponse.missingParam
         }
         try{
             // Check if the Secret Code is Authorized
             try{
-                const jwtData = await jwt.verify(reqData['secretCode'],process.env.JWTsecret)
+                const jwtData = await jwt.verify(reqData['token'],process.env.tokenSecret)
                 if (jwtData['email'] !== reqData['email']){
                     throw new Error("Invalid JWT")
                 }
             }catch(err){
-                return commonResposes.sendError("Not Authorized")
+                return commonResponse.custom(405,"Not Authorized")
             }
 
             // The Token is Ok, so Get The Login Info
             const loginInfo = await Database.getRepository(LoginRouter).findOneBy({email:reqData['email']})
             if (loginInfo === null){
-                return commonResposes.notFound
+                return commonResponse.notFound
             }
 
             // Change the Password
             loginInfo.password = await bcrypt.hash(reqData['newPassword'],10)
             await Database.getRepository(LoginRouter).save(loginInfo)
 
-            return commonResposes.sendData("Password Reseted Successfully")
+            return commonResponse.sendData("Password Reseted Successfully")
         }catch(err){
             console.log("Error!\n",err)
-            return commonResposes.Error
+            return commonResponse.Error
         }
     }
 }
--- src/Server/Login.ts
+++ src/Server/Login.ts
@@ -10,19 +10,25 @@ const router = Router()
 
 
 // Base endpoint to Test That API is Working
-router.post('/login',async (req,res)=>{
+router.post("/login",async (req,res)=>{
     const response = await  LoginFunctions.Login(req.body)
     res.status(response['status']).json(response['data'])
 })
 
-// General Signup Route for All (Doctors and Patient)
-router.post("/signup",async (req,res)=>{
-    const response = await LoginFunctions.Signup(req.body)
+// Signup Route For Doctors
+router.post("/doctor-signup",async (req,res)=>{
+    const response = await LoginFunctions.doctorSignup(req.body)
+    res.status(response['status']).json(response['data'])
+})
+
+// Signup Route For Patient
+router.post("/patient-signup",async (req,res)=>{
+    const response = await LoginFunctions.patientSignup(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Resend the Confirmation Code 
-router.post("/send-confirm-code",async (req,res)=>{
+router.post("/send-confirmation-code",async (req,res)=>{
     const response = await LoginFunctions.SendConfirmCode(req.body)
     res.status(response['status']).json(response['data'])
 })
@@ -33,33 +39,15 @@ router.post("/check-confirmation-code",async (req,res)=>{
     res.status(response['status']).json(response['data'])
 })
 
-// Adding Doctors Main Info like Name, Title, Description
-router.post("/doctor-main-info",async (req,res)=>{
-    const response = await LoginFunctions.addDoctorMainInfo(req.body)
-    res.status(response['status']).json(response['data'])
-})
-
-// Adding Patient Main Info Like Name, BirthDate
-router.post("/patient-main-info",async (req,res)=>{
-    const response = await LoginFunctions.addPatientMainInfo(req.body)
-    res.status(response['status']).json(response['data'])
-})
-
-// Send a Confirmation Code To User's Email
-router.post("/forget-password",async (req,res)=>{
-    const response = await LoginFunctions.SendConfirmCode(req.body)
-    res.status(response['status']).json(response['data'])
-})
-
-//Check if the sent Code is Correct
-router.post("/confirm-forget-password",async (req,res)=>{
-    const response = await LoginFunctions.ConfirmForgetPasswordCode(req.body)
+// Used To Verify Account With a Token
+router.post("/verify-account",async (req,res)=>{
+    const response = await LoginFunctions.confirmAccount(req.body)
     res.status(response['status']).json(response['data'])
 })
 
 // Reset The Password with Authorization Check
 router.post("/reset-password",async (req,res)=>{
-    const response = await LoginFunctions.ResetPassword(req.body)
+    const response = await LoginFunctions.forgetPassword(req.body)
     res.status(response['status']).json(response['data'])
 })
 
