1618c108c5e388aebc2eeff245177e394d19796e
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-03-07 20:08:06 +0200

    .

--- src/Middleware/Auth.ts
+++ src/Middleware/Auth.ts
@@ -4,14 +4,14 @@ var jwt = require('jsonwebtoken');
 export async function Authenticate(req,res,next){
     next()
 
-    // Commented For Testing
+    // // Commented For Testing
     // const token = req.headers['authorization']
     // const userRole = req.headers['userrole']
     // try{
     //     const jwtData = await jwt.verify(token,process.env.JWTsecret)
 
     //     if (jwtData['role'] !== userRole){
-    //         res.status(401).json("unauthorized")
+    //         throw Error;
     //     }else{
     //         next()
     //     }
