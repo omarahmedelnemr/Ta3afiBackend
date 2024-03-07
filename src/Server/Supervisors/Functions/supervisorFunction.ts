79008cfb7433c2c2af84d328157e9374591a2943
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-03-08 00:59:27 +0200

    Fixing Posts Response

--- src/Server/Supervisors/Functions/supervisorFunction.ts
+++ src/Server/Supervisors/Functions/supervisorFunction.ts
@@ -8,6 +8,7 @@ import checkUndefined from "../../../Public Functions/checkUndefined";
 import responseGenerator from "../../../Public Functions/responseGenerator";
 import NotificationFunctions from "../../User Info/Functions/NotificationFunctions";
 import communityFunctions from "../../Community/Functions/communityFunctions";
+import { PostImages } from "../../Community/Tables/PostImages";
 
 class SupervisorFunction{
 
@@ -84,12 +85,57 @@ class SupervisorFunction{
     }
 
     // Get All Un-Approved Posts
-    async getAllunapprovedPosts(){
+    async getAllunapprovedPosts(reqData){
+        var loadBlock = 1
+        if (reqData['loadBlock'] !== undefined && Number(reqData['loadBlock'])>1){
+            loadBlock = reqData['loadBlock']
+        }
         try{
 
-            // Getting all Un Approved Categories
-            const data = await Database.getRepository(Post).findBy({approved:false})
-            return responseGenerator.sendData(data)
+            var data =await  Database.getRepository(Post)
+            .createQueryBuilder("Post")
+            .innerJoinAndSelect("Post.patient", "patient")
+            .innerJoinAndSelect("Post.community", "community")
+            .where("Post.approved = false")
+            .andWhere("Post.deleted = false")
+            .select([
+              "Post.id as id",
+              "Post.date as date",
+              "Post.views as views",
+              "Post.edited as edited",
+              "Post.mainText as mainText",
+              "Post.AI_saftyRate as AI_saftyRate",
+              "Post.AI_saftyWord as AI_saftyWord",
+              "Post.hideIdentity as hideIdentity",
+              "community.name as community",
+              "patient.id as patientID",
+              "patient.name as userName",
+              "patient.profileImage as userProfileImage",
+            ])
+            .orderBy('Post.date', 'DESC')
+            .limit(15)
+            .offset(15 * (Number(loadBlock) - 1))
+
+            ////  Filtering Posts
+            // Getting a specific Community Posts
+            if (reqData["communityID"] !== undefined ){
+                data = data
+                .andWhere("community.id = :communityID", { communityID: reqData['communityID'] })
+                
+            }
+            // Getting Posts With a specific Text in them
+            if (reqData["searchText"] !== undefined ){
+                data = data
+                .andWhere("Post.mainText like :searchText", { searchText: "%" + reqData['searchText'] + "%" })
+            }
+            var allPosts = await data.getRawMany()
+
+            // Refactor the Json
+            for(var d =0;d<allPosts.length; d++){
+                allPosts[d]['images'] = await  Database.getRepository(PostImages).findBy({post:{id:allPosts[d].id}})
+
+            }
+            return responseGenerator.sendData(allPosts)
 
         }catch(err){
             console.log("Error!\n",err)
@@ -106,29 +152,20 @@ class SupervisorFunction{
             return responseGenerator.sendMissingParam(checkParam)
         }
         try{
+            // Get The Post Info
+            const postInfo = await Database.getRepository(Post).findOneBy({id:reqData['postID'],approved:false})
+            if (postInfo === null){
+                return responseGenerator.notFound
+            }
             if (reqData['approve']){
-
-                // Get The Post Info
-                const category = await Database.getRepository(Post).findOneBy({id:reqData['postID'],approved:false})
-                if (category === null){
-                    return responseGenerator.notFound
-                }
-
                 // Approve The Post and Save it
-                category.approved = true
-                await Database.getRepository(Post).save(category)
+                postInfo.approved = true
                 
             }else{
-
-                // Remove The Post
-                await Database
-                .getRepository(Post)
-                .createQueryBuilder('Post')
-                .delete()
-                .from(Post)
-                .where("id = :postID", { postID: reqData['postID'] })
-                .execute()
+                // Edit the Post To Be Deleted and Save it
+                postInfo.deleted = true
             }
+            await Database.getRepository(Post).save(postInfo)
 
             // Send in-app Notification
             const writer = await Database.getRepository(Post)
@@ -136,7 +173,7 @@ class SupervisorFunction{
             .innerJoinAndSelect("Post.patient",'patient')
             .where("Post.id = :postID",{postID:reqData['postID']})
             .getOne()
-            const stat = reqData['approve']? "Approved":"Disapproved"
+            const stat = reqData['approve']? "Approved":`Disapproved (${reqData['reason']})`
             await NotificationFunctions.sendPatientNotification(writer.patient.id,"System",`Your Post Has Been ${stat}`,'')
 
 
@@ -151,16 +188,16 @@ class SupervisorFunction{
     // Remove a Post From Posts Lists (With a Message)
     async Remove_post(reqData){
         // Check Parameter Existence
-        const checkParam = checkUndefined(reqData,['postID','superID','passcode'])
+        const checkParam = checkUndefined(reqData,['postID','reason'])
         if (checkParam){
             return responseGenerator.sendMissingParam(checkParam)
         }
         try{
-            // Check Supervisor Auth
-            const supervisor = await Database.getRepository(Supervisor).findOneBy({id:reqData['superID'],passcode:reqData['passcode']})
-            if(supervisor === null){
-                return responseGenerator.custom(405,"unAuthorized")
-            }
+            // // Check Supervisor Auth
+            // const supervisor = await Database.getRepository(Supervisor).findOneBy({id:reqData['superID'],passcode:reqData['passcode']})
+            // if(supervisor === null){
+            //     return responseGenerator.custom(405,"unAuthorized")
+            // }
 
             // Get The Post Info
             const postInfo = await Database.getRepository(Post).findOneBy({id:reqData['postID']})
@@ -179,7 +216,7 @@ class SupervisorFunction{
             .where("Post.id = :postID",{postID:reqData['postID']})
             .getOne()
             const notifyText = writer.mainText.length > 20 ? writer.mainText.substring(0, 20) + '...' : writer.mainText;
-            await NotificationFunctions.sendPatientNotification(writer.patient.id,"System","Your Post Had Been Deleted Due To Community Guidelines",notifyText)
+            await NotificationFunctions.sendPatientNotification(writer.patient.id,"System",`Your Post Had Been Deleted Due To Community Guidelines (${reqData['reason']})`,notifyText)
 
             return responseGenerator.done
 
@@ -192,33 +229,35 @@ class SupervisorFunction{
     // Remove a Post From Posts Lists (With a Message)
     async Remove_comment(reqData){
         // Check Parameter Existence
-        const checkParam = checkUndefined(reqData,['commentID',"patientID",'superID','passcode'])
+        const checkParam = checkUndefined(reqData,['commentID',"patientID",'reason'])
         if (checkParam){
             return responseGenerator.sendMissingParam(checkParam)
         }
         try{
-            // Check Supervisor Auth
-            const supervisor = await Database.getRepository(Supervisor).findOneBy({id:reqData['superID'],passcode:reqData['passcode']})
-            if(supervisor === null){
-                return responseGenerator.custom(405,"unAuthorized")
-            }
+            // // Check Supervisor Auth
+            // const supervisor = await Database.getRepository(Supervisor).findOneBy({id:reqData['superID'],passcode:reqData['passcode']})
+            // if(supervisor === null){
+            //     return responseGenerator.custom(405,"unAuthorized")
+            // }
             
             // Get Comment Info 
-            const comment = await Database.getRepository(PostComment)
+            const comment = await Database.getRepository(PostComment)//.findOneBy({id:reqData['commentID']})
             .createQueryBuilder("PostComment")
             .innerJoinAndSelect("PostComment.patient",'patient')
-            .where("PostComment.commentID = :commentID",{postID:reqData['commentID']})
+            .where("PostComment.id = :commentID",{commentID:reqData['commentID']})
             .getOne()
 
-            // Delete The Comment
-            const rmv = await communityFunctions.RemoveComment(reqData)
-            if (rmv.status !== 200){
-                return rmv
-            }
+            // // Delete The Comment
+            // const rmv = await communityFunctions.RemoveComment(reqData)
+            // if (rmv.status !== 200){
+            //     return rmv
+            // }
+            comment.deleted = true
+            await Database.getRepository(PostComment).save(comment)
 
             // Send in-app Notification
             const notifyText = comment.comment.length > 20 ? comment.comment.substring(0, 20) + '...' : comment.comment;
-            await NotificationFunctions.sendPatientNotification(comment.patient.id,"System","Your Comment Had Been Deleted Due To Community Guidelines",notifyText)
+            await NotificationFunctions.sendPatientNotification(comment.patient.id,"System",`Your Comment Had Been Deleted Due To Community Guidelines (${reqData['reason']})`,notifyText)
 
             return responseGenerator.done
 
